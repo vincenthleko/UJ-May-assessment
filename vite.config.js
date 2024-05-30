7b6749fd8c37b16316583807bae59b8570234b03
Vincent~ <vincenthleko95@gmail.com>
2024-05-30 15:58:08 +0200

    personal cv

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
 
 export default {
+  base : '/responsive_cv/',
   plugins: [
     handlebars({
-      context: {}, // replace the curly brackets with data.json
+      context: data // replace the curly brackets with data.json
     }),
   ],
 };
